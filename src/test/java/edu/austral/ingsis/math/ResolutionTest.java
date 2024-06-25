8e8fa2b1810850d5c88c3444fdc93253bb32ca9a
rcastromartinez <rcastromartinez@mail.austral.edu.ar>
2024-06-24 21:46:58 -0300

    Visitors test 4/4

--- src/test/java/edu/austral/ingsis/math/ResolutionTest.java
+++ src/test/java/edu/austral/ingsis/math/ResolutionTest.java
@@ -3,14 +3,19 @@ package edu.austral.ingsis.math;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.austral.ingsis.math.visitor.Visitor;
+import edu.austral.ingsis.math.visitor.VisitorEvaluate;
 import org.junit.jupiter.api.Test;
 
 public class ResolutionTest {
 
+  private final Visitor<Double> visitor = new VisitorEvaluate();
+
   /** Case 1 + 6 */
   @Test
   public void shouldResolveSimpleFunction1() {
-    final Double result = 7d;
+
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Add(new edu.austral.ingsis.math.visitor.Num(1.0), new edu.austral.ingsis.math.visitor.Num(6.0)));
 
     assertThat(result, equalTo(7d));
   }
@@ -18,7 +23,7 @@ public class ResolutionTest {
   /** Case 12 / 2 */
   @Test
   public void shouldResolveSimpleFunction2() {
-    final Double result = 6d;
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Div(new edu.austral.ingsis.math.visitor.Num(12.0), new edu.austral.ingsis.math.visitor.Num(2.0)));
 
     assertThat(result, equalTo(6d));
   }
@@ -26,7 +31,7 @@ public class ResolutionTest {
   /** Case (9 / 2) * 3 */
   @Test
   public void shouldResolveSimpleFunction3() {
-    final Double result = 13.5;
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Multi(new edu.austral.ingsis.math.visitor.fun.Div(new edu.austral.ingsis.math.visitor.Num(9.0), new edu.austral.ingsis.math.visitor.Num(2.0)), new edu.austral.ingsis.math.visitor.Num(3.0)));
 
     assertThat(result, equalTo(13.5d));
   }
@@ -34,7 +39,7 @@ public class ResolutionTest {
   /** Case (27 / 6) ^ 2 */
   @Test
   public void shouldResolveSimpleFunction4() {
-    final Double result = 20.25;
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Power(new edu.austral.ingsis.math.visitor.fun.Div(new edu.austral.ingsis.math.visitor.Num(27.0), new edu.austral.ingsis.math.visitor.Num(6.0)), new edu.austral.ingsis.math.visitor.Num(2.0)));
 
     assertThat(result, equalTo(20.25d));
   }
@@ -42,7 +47,7 @@ public class ResolutionTest {
   /** Case 36 ^ (1/2) */
   @Test
   public void shouldResolveSimpleFunction5() {
-    final Double result = 6d;
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Power(new edu.austral.ingsis.math.visitor.Num(36.0), new edu.austral.ingsis.math.visitor.fun.Div(new edu.austral.ingsis.math.visitor.Num(1.0), new edu.austral.ingsis.math.visitor.Num(2.0))));
 
     assertThat(result, equalTo(6d));
   }
@@ -50,7 +55,7 @@ public class ResolutionTest {
   /** Case |136| */
   @Test
   public void shouldResolveSimpleFunction6() {
-    final Double result = 136d;
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Abs(new edu.austral.ingsis.math.visitor.Num(136.0)));
 
     assertThat(result, equalTo(136d));
   }
@@ -58,7 +63,7 @@ public class ResolutionTest {
   /** Case |-136| */
   @Test
   public void shouldResolveSimpleFunction7() {
-    final Double result = 136d;
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Abs(new edu.austral.ingsis.math.visitor.fun.Sub(new edu.austral.ingsis.math.visitor.Num(0.0), new edu.austral.ingsis.math.visitor.Num(136.0))));
 
     assertThat(result, equalTo(136d));
   }
@@ -66,7 +71,7 @@ public class ResolutionTest {
   /** Case (5 - 5) * 8 */
   @Test
   public void shouldResolveSimpleFunction8() {
-    final Double result = 0d;
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Multi(new edu.austral.ingsis.math.visitor.fun.Sub(new edu.austral.ingsis.math.visitor.Num(5.0), new edu.austral.ingsis.math.visitor.Num(5.0)), new edu.austral.ingsis.math.visitor.Num(8.0)));
 
     assertThat(result, equalTo(0d));
   }
--- src/test/java/edu/austral/ingsis/math/ResolutionWithVariablesTest.java
+++ src/test/java/edu/austral/ingsis/math/ResolutionWithVariablesTest.java
@@ -3,14 +3,20 @@ package edu.austral.ingsis.math;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.austral.ingsis.math.visitor.VisitorEvaluate;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 public class ResolutionWithVariablesTest {
+
+  private final VisitorEvaluate VariablesVisitor = new VisitorEvaluate();
 
   /** Case 1 + x where x = 3 */
   @Test
   public void shouldResolveFunction1() {
-    final Double result = 4d;
+    VisitorEvaluate visitor = VariablesVisitor.withVariable(Map.of("x", 3d));
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Add(new edu.austral.ingsis.math.visitor.Num(1.0), new edu.austral.ingsis.math.visitor.Variable("x")));
 
     assertThat(result, equalTo(4d));
   }
@@ -18,7 +24,8 @@ public class ResolutionWithVariablesTest {
   /** Case 12 / div where div = 4 */
   @Test
   public void shouldResolveFunction2() {
-    final Double result = 3d;
+    VisitorEvaluate visitor = VariablesVisitor.withVariable(Map.of("div", 4d));
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Div(new edu.austral.ingsis.math.visitor.Num(12.0), new edu.austral.ingsis.math.visitor.Variable("div")));
 
     assertThat(result, equalTo(3d));
   }
@@ -26,7 +33,8 @@ public class ResolutionWithVariablesTest {
   /** Case (9 / x) * y where x = 3 and y = 4 */
   @Test
   public void shouldResolveFunction3() {
-    final Double result = 12d;
+    VisitorEvaluate visitor = VariablesVisitor.withVariable(Map.of("x", 3d, "y", 4d));
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Multi(new edu.austral.ingsis.math.visitor.fun.Div(new edu.austral.ingsis.math.visitor.Num(9.0), new edu.austral.ingsis.math.visitor.Variable("x")), new edu.austral.ingsis.math.visitor.Variable("y")));
 
     assertThat(result, equalTo(12d));
   }
@@ -34,7 +42,8 @@ public class ResolutionWithVariablesTest {
   /** Case (27 / a) ^ b where a = 9 and b = 3 */
   @Test
   public void shouldResolveFunction4() {
-    final Double result = 27d;
+    VisitorEvaluate visitor = VariablesVisitor.withVariable(Map.of("a", 9d, "b", 3d));
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Power(new edu.austral.ingsis.math.visitor.fun.Div(new edu.austral.ingsis.math.visitor.Num(27.0), new edu.austral.ingsis.math.visitor.Variable("a")), new edu.austral.ingsis.math.visitor.Variable("b")));
 
     assertThat(result, equalTo(27d));
   }
@@ -42,7 +51,8 @@ public class ResolutionWithVariablesTest {
   /** Case z ^ (1/2) where z = 36 */
   @Test
   public void shouldResolveFunction5() {
-    final Double result = 6d;
+    VisitorEvaluate visitor = VariablesVisitor.withVariable(Map.of("z", 36d));
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Power(new edu.austral.ingsis.math.visitor.Variable("z"), new edu.austral.ingsis.math.visitor.fun.Div(new edu.austral.ingsis.math.visitor.Num(1.0), new edu.austral.ingsis.math.visitor.Num(2.0))));
 
     assertThat(result, equalTo(6d));
   }
@@ -50,7 +60,8 @@ public class ResolutionWithVariablesTest {
   /** Case |value| - 8 where value = 8 */
   @Test
   public void shouldResolveFunction6() {
-    final Double result = 0d;
+    VisitorEvaluate visitor = VariablesVisitor.withVariable(Map.of("value", 8d));
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Sub(new edu.austral.ingsis.math.visitor.fun.Abs(new edu.austral.ingsis.math.visitor.Variable("value")), new edu.austral.ingsis.math.visitor.Num(8.0)));
 
     assertThat(result, equalTo(0d));
   }
@@ -58,7 +69,8 @@ public class ResolutionWithVariablesTest {
   /** Case |value| - 8 where value = 8 */
   @Test
   public void shouldResolveFunction7() {
-    final Double result = 0d;
+    VisitorEvaluate visitor = VariablesVisitor.withVariable(Map.of("value", 8d));
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Sub(new edu.austral.ingsis.math.visitor.fun.Abs(new edu.austral.ingsis.math.visitor.Variable("value")), new edu.austral.ingsis.math.visitor.Num(8.0)));
 
     assertThat(result, equalTo(0d));
   }
@@ -66,7 +78,8 @@ public class ResolutionWithVariablesTest {
   /** Case (5 - i) * 8 where i = 2 */
   @Test
   public void shouldResolveFunction8() {
-    final Double result = 24d;
+    VisitorEvaluate visitor = VariablesVisitor.withVariable(Map.of("i", 2d));
+    final Double result = visitor.visit(new edu.austral.ingsis.math.visitor.fun.Multi(new edu.austral.ingsis.math.visitor.fun.Sub(new edu.austral.ingsis.math.visitor.Num(5.0), new edu.austral.ingsis.math.visitor.Variable("i")), new edu.austral.ingsis.math.visitor.Num(8.0)));
 
     assertThat(result, equalTo(24d));
   }
